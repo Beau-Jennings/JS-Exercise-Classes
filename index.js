bbdce0a370525ac8e36877504e76d1f955cb3fc5
Beau Jennings <redalthazar@gmail.com>
2020-11-12 17:33:47 -0600

    finished task 2, fix task 3

--- index.js
+++ index.js
@@ -86,22 +86,24 @@ class Car {
     this.milesPerGallon = milesPerGallon;
     this.tank = 0;
     this.odometer = 0;
-  }
-}  
+  }  
 
-  Car.prototype.drive = function(distance){
-    this.odometer = distance + this.odometer;
-    this.tank = distance / this.milesPerGallon;
-    if (this.tank = 0){
+  fill(gallons){
+    this.tank = this.tank + gallons
+  }
+  drive(distance){
+    const distanceDriven = this.milesPerGallon * this.tank;
+    if (distanceDriven > distance){
+      this.tank -= (distance/this.milesPerGallon)
+      this.odometer += distance;
+    }  
+    else{
+      this.odometer += distanceDriven;
+      this.tank = 0;
       return `I ran out of fuel at ${this.odometer} miles!`
     }
   }
-
-  Car.prototype.fill = function(gallons){
-    if (this.tank <100){
-      this.tank = gallons + this.tank
-    }
-  }
+}
 
 
 
@@ -120,7 +122,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes){
 
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -137,8 +147,22 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(attributes){
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase
+  }
+  
+  demo(subject){
+    this.subject = "Lambda";
+    return `Today we are learning about ${subject}`
+  }
 
+  grade(student, subject){
+    return `${student.name} recieves a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -156,10 +180,20 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(attributes){
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
+  listSubjects(){
+    return "Loving HTML, CSS, JS, Node, Redux!";
+  }
+  PRAssignment(student, subject){
+    return `${student.name} has submitted a PR for ${subject}`;
+  }
 }
-
 /*
   TASK 6
     - Write a ProjectManager class extending Instructor.
