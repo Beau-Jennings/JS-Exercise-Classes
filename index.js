088e137606e3582e58786552d00e1d8d05ad839c
Beau Jennings <redalthazar@gmail.com>
2020-11-12 18:51:46 -0600

    finished

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ class Person {
       this.stomach.push(edible);
     }
   }
-  poop(empty){
+  poop(){
     if (this.stomach.length === 10){
       this.stomach = [];
     }
@@ -190,8 +190,11 @@ class Student extends Lambdasian {
   listSubjects(){
     return "Loving HTML, CSS, JS, Node, Redux!";
   }
-  PRAssignment(student, subject){
-    return `${student.name} has submitted a PR for ${subject}`;
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
   }
 }
 /*
@@ -207,8 +210,19 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(attributes){
+    super(attributes);{
+      this.gradClassName = attributes.gradClassName;
+      this.favInstructor = attributes.favInstructor;
+    }
+  }
+  standUp(Web38){
+    return `${this.name} announces to ${Web38}, @channel standy times`;
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
 
 /*
